6d234d3a5605049fcabf7c655be774f14221049c
Mohammad Amin Parvanian <amin_prvn@outlook.com>
2021-01-27 12:22:23 +0330

    save and update for models

--- app/serializers.py
+++ app/serializers.py
@@ -9,12 +9,28 @@ class ClinicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Clinic
         fields = ('email', 'password', 'name', 'address', 'phone', 'description')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'email': {'write_only': True},
+        }
 
-    # def create(self, validated_data):
-    #     clinic = Clinic(**validated_data)
-    #     clinic.set_password()
-    #     clinic.save()
-    #     return clinic
+    def create(self, validated_data):
+        clinic = Clinic(**validated_data)
+        clinic.set_password()
+        clinic.save()
+        return clinic
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        if validated_data.get('password'):
+            instance.password = validated_data.get('password')
+            instance.set_password()
+        instance.name = validated_data.get('name', instance.name)
+        instance.address = validated_data.get('address', instance.address)
+        instance.phone = validated_data.get('phone', instance.phone)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+        return instance
 
 
 class PatientSerializer(serializers.ModelSerializer):
@@ -24,5 +40,25 @@ class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
         fields = ('email', 'password', 'name', 'phone')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'email': {'write_only': True},
+        }
+
+    def create(self, validated_data):
+        patient = Patient(**validated_data)
+        patient.set_password()
+        patient.save()
+        return patient
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        if validated_data.get('password'):
+            instance.password = validated_data.get('password')
+            instance.set_password()
+        instance.name = validated_data.get('name', instance.name)
+        instance.phone = validated_data.get('phone', instance.phone)
+        instance.save()
+        return instance
 
 
