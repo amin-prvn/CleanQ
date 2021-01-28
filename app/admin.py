ad0504e1ac7f1de8cf1876651d76139865866468
Mohammad Amin Parvanian <amin_prvn@outlook.com>
2021-01-28 17:50:30 +0330

    register simple admin

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
+
 from .models import *
 
 
 @admin.register(Clinic)
-class youtuber_admin(admin.ModelAdmin):
+class ClinicAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
     pass
