2a06db9b186cde88557ab8b5913efcd6d497fe55
Mohammad Amin Parvanian <amin_prvn@outlook.com>
2021-01-29 18:42:16 +0330

    set time for reservation

--- app/serializers.py
+++ app/serializers.py
@@ -4,8 +4,10 @@ from .models import Reservation, Clinic, Patient
 from .validators import phone_number
 
 # import pdb; pdb.set_trace()
-import  datetime
-START_TIME = datetime.time(hour=8)
+from  datetime import datetime, timedelta
+START_TIME = 10
+END_TIME = 20
+DELTA_TIME = 30
 
 class ClinicSerializer(serializers.ModelSerializer):
 
@@ -63,8 +65,87 @@ class ReservationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Reservation
-        fields = ('clinic', 'description')
+        fields = ('patient', 'clinic', 'time', 'description')
+        extra_kwargs = {
+            'patient': {'read_only': True},
+            'time': {'read_only': True},
+        }
 
     def create(self, validated_data):
+        clinic_id = validated_data['clinic'].id
+        patient = Patient.objects.get(pk=validated_data['patient'])
+        now = datetime.now()
+        try:
+            last_reservation = Reservation.objects.filter(clinic=clinic_id).latest('time')
+
+            # Today
+            if last_reservation.time.date() == now.date():
+
+                if last_reservation.time.hour >= END_TIME:
+                    time = tommorow(last_reservation.time)
+
+                elif last_reservation.time.hour >= now.hour:
+                    time = before_or_after_delta(last_reservation.time)
+                
+                elif last_reservation.time.hour < now.hour:
+                    time = before_or_after_delta(now)
+
+            # Before Today
+            elif last_reservation.time.date() < now.date():
+
+                if now.hour >= END_TIME:
+                    time = tommorow(now)
+                
+                else:
+                    time = before_or_after_delta(now)
+
+            # After Today
+            elif last_reservation.time.date() > now.date():
+
+                if last_reservation.time.hour >= END_TIME:
+                    time = tommorow(last_reservation.time)
+
+                else:
+                    time = before_or_after_delta(last_reservation.time)
+                    print(time)
+    
+            reservation = Reservation.objects.create(
+                patient=patient, 
+                clinic=validated_data['clinic'],
+                description=validated_data.get('description'), 
+                time=time
+                )
+        except Reservation.DoesNotExist:
+
+            reservation = Reservation.objects.create(
+                patient=patient, 
+                clinic=validated_data['clinic'],
+                description=validated_data.get('description'), 
+                time=datetime(
+                    year=now.year,
+                    month=now.month,
+                    day=now.day+1,
+                    hour=START_TIME
+                    )
+                )
+        return reservation
+
+
+def before_or_after_delta(time):
+    if time.minute >= DELTA_TIME :
+        print('after')
+        time += timedelta(hours=1)
+        return time.replace(minute=0, second=0, microsecond=0)
+
+    else:
+        print('before')
+        return time.replace(minute=DELTA_TIME, second=0, microsecond=0)
+
+def tommorow(time):
+    print('tommorow')
+    time += timedelta(days=1)
+    return time.replace(hour=START_TIME, minute=0, second=0, microsecond=0)
+    
+
 
         
