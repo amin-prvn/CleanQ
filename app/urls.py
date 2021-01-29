8afae93ffa13b975c35a6c0e970ad05882b1070e
Mohammad Amin Parvanian <amin_prvn@outlook.com>
2021-01-29 23:14:19 +0330

    add some paths

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import  ClinicView, PatientView
+from .views import ClinicView, PatientView, GetClinic, ReservationView
 
 urlpatterns = [
     path('clinic', ClinicView.as_view()),
     path('patient', PatientView.as_view()),
+    path('get-clinics', GetClinic.as_view()),
+    path('reserve', ReservationView.as_view()),
 ]
